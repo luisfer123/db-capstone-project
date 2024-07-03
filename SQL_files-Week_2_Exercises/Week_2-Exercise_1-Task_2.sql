f54f453f6ae03248dd2eba18fa5e102327f63935
luisfer123 <luisfermtnz@gmail.com>
2024-07-02 21:03:10 -0600

    Database client added

--- SQL_files-Week_2_Exercises/Week_2-Exercise_1-Task_2.sql
+++ SQL_files-Week_2_Exercises/Week_2-Exercise_1-Task_2.sql
@@ -3,7 +3,7 @@
 
 SELECT C.customer_id, C.full_name, O.order_id, O.total_cost, M.name, MI.course_name, MI.starter_name
 	FROM Customer C
-    INNER JOIN Orders O ON C.customer_id = O.Customer_customer_id
+    INNER JOIN Orders O ON C.customer_id = O.customer_id
 	INNER JOIN Menu M ON O.order_id = M.order_id
     INNER JOIN Menu_item MI ON M.menu_id = MI.menu_item_id
     ORDER BY O.total_cost;
